228e3a54d6fbb8e1a62848f89e42215c31b28bb4
toms92 <tommaso.casiraghi@live.com>
2026-01-30 11:10:55 +0100

    project structure

---
+++ src/main/java/com/example/spotifyindiano/SpotifyIndianoApplication.java
@@ -0,0 +1,13 @@
+package com.example.spotifyindiano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyIndianoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpotifyIndianoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spotifyindiano/SpotifyIndianoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spotifyindiano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyIndianoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
